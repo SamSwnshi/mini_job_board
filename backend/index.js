81d15a8dd8323e883930b900e3b667143998c339
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2025-11-04 19:44:43 +0530

    auth routes

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import configDB from './db/config.js'
+import authRoutes from './routes/auth.js'
 
 dotenv.config()
 const app = express()
@@ -10,6 +11,7 @@ const port = process.env.PORT || 8080;
 app.use(cors())
 app.use(express.json()) 
 
+app.use('/api',authRoutes)
 app.listen(port,()=>{
     configDB()
     console.log(`Server is Running on the PORT: ${port}`)
