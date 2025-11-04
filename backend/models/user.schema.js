1fc472cd82cf69e9947f02e4b31c45398465b92b
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2025-11-04 19:35:12 +0530

    schema

---
+++ backend/models/user.schema.js
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const userSchema = new mongoose.Schema({
+    username: { 
+        type: String, 
+        required: true, 
+        unique: true, 
+        trim: true 
+    },
+    email: { 
+        type: String, 
+        required: true, 
+        unique: true, 
+        trim: true, 
+        lowercase: true 
+    },
+    password: { 
+        type: String, 
+        required: true 
+    },
+
+    role: { 
+        type: String, 
+        enum: ['applicant', 'employer'], 
+        default: 'applicant' 
+    },
+    companyName: { 
+        type: String,
+        required: function() { return this.role === 'employer'; } 
+    },
+}, { timestamps: true });
+
+
+userSchema.pre('save', async function(next) {
+    if (this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+    next();
+});
+
+const User = mongoose.model('User', userSchema);
+export default User;
